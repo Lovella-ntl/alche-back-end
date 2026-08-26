3a4edcab66d4fe3c673547538a4ca092ec29ec36
Lovella-ntl <l.nkusi@alustudent.com>
2026-08-26 06:30:44 +0000

    updated 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-import sys
 import requests
-
-
+import sys
+ 
+ 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
-
     base_url = "https://jsonplaceholder.typicode.com"
-
-    user_url = "{}/users/{}".format(base_url, employee_id)
-    todos_url = "{}/todos".format(base_url)
-
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    todos_response = requests.get(todos_url, params={"userId": employee_id})
-    todos_data = todos_response.json()
-
-    total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get("completed") is True]
+ 
+    user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
+    employee_name = user.get("name")
+ 
+    todos = requests.get("{}/todos".format(base_url)).json()
+    employee_todos = [
+        task for task in todos if task.get("userId") == employee_id
+    ]
+ 
+    total_tasks = len(employee_todos)
+    done_tasks = [
+        task for task in employee_todos if task.get("completed") is True
+    ]
     number_of_done_tasks = len(done_tasks)
-
+ 
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, number_of_done_tasks, total_tasks))
-
+ 
     for task in done_tasks:
         print("\t {}".format(task.get("title")))
